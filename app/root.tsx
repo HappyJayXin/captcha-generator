910a9e1d83019d57800cdec9eab81b264d1625ef
Jay <happyjayxin@gmail.com>
2024-11-03 10:30:32 +0800

    chore(root): add reset normalize stylesheets to root links

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,8 @@
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
+import resetStyle from "./styles/reset.css?url";
+import normalizeStyle from "./styles/normalize.css?url";
+import baseStyle from "./styles/global.css?url";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -12,6 +15,9 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  { rel: "stylesheet", href: resetStyle },
+  { rel: "stylesheet", href: normalizeStyle },
+  { rel: "stylesheet", href: baseStyle },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
